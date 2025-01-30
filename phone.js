1c722ece6bcada498af24bf5441e0b681514877b
shamir <shamir@kali>
2025-01-30 07:45:05 +0530

    large screen keyboard

--- phone.js
+++ phone.js
@@ -18,7 +18,7 @@ function createControlButtons() {
             id: "left",
             text: "←",
             style: isLargeScreen
-                ? "bottom: 60px; left: 21%; transform: translateY(-50%);"  // Adjusted for large-screen phones
+                ? "bottom: 60px; left: 21.3%; transform: translateY(-50%);"  // Adjusted for large-screen phones
                 : "bottom: 60px; left: 8.55%; transform: translateY(-50%);"
         },
         {
