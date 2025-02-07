887d6dd39bc527a79feaf94b4adfc371b48ea09a
shamir <shamir@kali>
2025-02-08 00:21:04 +0530

    phone keys

--- phone.js
+++ phone.js
@@ -115,7 +115,7 @@ function addSwipeListeners() {
 
         if (Math.abs(diffX) > Math.abs(diffY)) {
             // Horizontal swipe
-            if (diffX > 160) {
+            if (diffX > 260) {
                 console.log("Swiped Left (Triggering 'ArrowLeft')");
                 triggerKey("swipeLeft", "keydown");
                 setTimeout(() => triggerKey("swipeLeft", "keyup"), 100);
