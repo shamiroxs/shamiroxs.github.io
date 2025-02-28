cbfe0483a06e64997b7736e6fb4c90ec5f35b5db
shamiroxs <shamiroxs@gmail.com>
2025-02-28 08:14:54 +0530

    me section

--- me.js
+++ me.js
@@ -4,29 +4,29 @@ export async function drawMe(scene, assets){
 
     //GitHub
     let logo = assets[17].scene.clone();
-    logo.scale.set(1.5, 1.5, 1.5);
+    logo.scale.set(1, 1, 1);
     logo.name = "logo 1";
-    logo.position.set(-0.4, 2, -29);
+    logo.position.set(-0.4, 6, -29);
     scene.add(logo);
 
     //LInkedIn
     logo = assets[18].scene.clone();
     logo.scale.set(1, 1, 1);
     logo.name = "logo 2";
-    logo.position.set(-0.4, 2, -39);
+    logo.position.set(-0.4, 9, -40);
     scene.add(logo);
 
     //Whatsapp
     logo = assets[19].scene.clone();
     logo.scale.set(1, 1, 1);
     logo.name = "logo 3";
-    logo.position.set(-0.4, 2, -49);
+    logo.position.set(-0.4, 12, -51);
     scene.add(logo);
 
     //Gmail
     logo = assets[20].scene.clone();
-    logo.scale.set(1, 1, 1);
+    logo.scale.set(6, 6, 6);
     logo.name = "logo 4";
-    logo.position.set(-0.4, 2, -59);
+    logo.position.set(-0.4, 20, -62);
     scene.add(logo);
 }
